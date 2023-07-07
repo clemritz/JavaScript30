614949a247f95b4a1611ff34c894fab548a5ba05
Clément Ritz <clement.ritz@orange.fr>
2023-07-07 18:06:09 +0200

    updateButton function

--- 11 - Custom Video Player/scripts.js
+++ 11 - Custom Video Player/scripts.js
@@ -6,7 +6,7 @@ const toggle = player.querySelector(".toggle");
 const skipButton = player.querySelector("[data-skip]");
 const ranges = player.querySelector(".player__slider");
 
-// BUILD FUNCTIONS HERE
+// Fonctions ici
 function togglePlay() {
   if (video.paused) {
     video.play();
@@ -15,5 +15,13 @@ function togglePlay() {
   }
 }
 
+function updateButton() {
+  const icon = this.paused ? '<i class="fa-solid fa-play"></i>' : '<i class="fa-solid fa-pause"></i>';
+  toggle.innerHTML = icon;
+}
+
 // Ajoutez cet événement au bouton de lecture/pause
 toggle.addEventListener("click", togglePlay);
+video.addEventListener("click", togglePlay);
+video.addEventListener("play", updateButton);
+video.addEventListener("pause", updateButton);
